0538a63c79552d1397a4c3dc8d73b8d7221bebd6
MaizeDaniele <daniele.caschili@outlook.com>
2022-09-30 20:35:40 +0200

    Added plugin

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # GameDevToolbox
 Unreal Engine plugin containing various utilities modules.
+
+## GameDevToolboxUI
+Module containing utilities classes for UI. It is based on the experimental CommonUI plugin.
---
+++ Source/GameDevToolboxUI/GameDevToolboxUI.build.cs
@@ -0,0 +1,20 @@
+using UnrealBuildTool;
+
+public class GameDevToolboxUI : ModuleRules
+{
+    public GameDevToolboxUI(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PrivateDependencyModuleNames.AddRange(new string[] {
+            
+        });
+
+        PublicDependencyModuleNames.AddRange(new string[] {
+            "Core",
+            "CoreUObject",
+            "Engine",
+            "UMG"            
+            });
+    }
+}
