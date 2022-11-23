fb9f9e21eaa3e24fd597f8b9d48ee6e17dc06a71
MaizeDaniele <daniele.caschili@outlook.com>
2022-11-23 22:01:32 +0100

    Added GameDevToolboxCore

---
+++ Source/GameDevToolboxCore/GameDevToolboxCore.build.cs
@@ -0,0 +1,19 @@
+using UnrealBuildTool;
+
+public class GameDevToolboxCore : ModuleRules
+{
+    public GameDevToolboxCore(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PrivateDependencyModuleNames.AddRange(new string[] {
+            
+        });
+
+        PublicDependencyModuleNames.AddRange(new string[] {
+            "Core",
+            "CoreUObject",
+            "Engine"                       
+            });
+    }
+}
