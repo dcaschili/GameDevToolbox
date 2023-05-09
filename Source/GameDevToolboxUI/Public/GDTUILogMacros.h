3ee8dff81169174ea87a9de58ef7f36dbdd45fc7
MaizeDaniele <daniele.caschili@outlook.com>
2023-05-09 22:29:43 +0200

    Fix problem with line macro

--- Source/GameDevToolboxUI/Public/GDTUILogMacros.h
+++ Source/GameDevToolboxUI/Public/GDTUILogMacros.h
@@ -2,4 +2,4 @@
 
 #define GDTUI_SHORT_LOG(LogCategory, VerbosityLevel, Format, ...) UE_LOG(LogCategory, VerbosityLevel, TEXT("%s: %s"), *FString(__FILE__), Format, ##__VA_ARGS__ )
 
-#define GDTUI_LOG(LogCategory, VerbosityLevel, Format, ...) UE_LOG(LogCategory, VerbosityLevel, TEXT("%s %s (%d): %s"), *FString(__FILE__), *FString(__FUNCTION__), *FString::FromInt(__LINE__), Format, ##__VA_ARGS__ )
+#define GDTUI_LOG(LogCategory, VerbosityLevel, Format, ...) UE_LOG(LogCategory, VerbosityLevel, TEXT("%s %s (%d): %s"), *FString(__FILE__), *FString(__FUNCTION__), __LINE__, Format, ##__VA_ARGS__ )
