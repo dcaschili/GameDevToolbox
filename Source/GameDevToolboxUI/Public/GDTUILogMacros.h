96005afdcec818ea8359ba13fae86e229c93d094
MaizeDaniele <daniele.caschili@outlook.com>
2023-05-17 08:17:40 +0200

    Added print to screen debug macro

--- Source/GameDevToolboxUI/Public/GDTUILogMacros.h
+++ Source/GameDevToolboxUI/Public/GDTUILogMacros.h
@@ -3,3 +3,9 @@
 #define GDTUI_SHORT_LOG(LogCategory, VerbosityLevel, Format, ...) UE_LOG(LogCategory, VerbosityLevel, TEXT("%s: %s"), *FString(__FILE__), *FString::Printf(Format, ##__VA_ARGS__ ))
 
 #define GDTUI_LOG(LogCategory, VerbosityLevel, Format, ...) UE_LOG(LogCategory, VerbosityLevel, TEXT("%s %s (%d): %s"), *FString(__FILE__), *FString(__FUNCTION__), __LINE__, *FString::Printf(Format, ##__VA_ARGS__ ) )
+
+#define GDTUI_PRINT_TO_SCREEN_LOG(Format, ...) if(GEngine) GEngine->AddOnScreenDebugMessage(-1, 10.0f, FColor::Green, FString::Printf(Format, ##__VA_ARGS__))
+
+#define GDTUI_PRINT_TO_SCREEN_WARN(Format, ...) if(GEngine) GEngine->AddOnScreenDebugMessage(-1, 10.0f, FColor::Yellow, FString::Printf(Format, ##__VA_ARGS__))
+
+#define GDTUI_PRINT_TO_SCREEN_ERROR(Format, ...) if(GEngine) GEngine->AddOnScreenDebugMessage(-1, 10.0f, FColor::Red, FString::Printf(Format, ##__VA_ARGS__))
